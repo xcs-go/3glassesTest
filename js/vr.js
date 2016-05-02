9515be2ac7721e3db98d420e73a965d6c9268261
xcs-go <13162927913@163.com>
2016-05-03 00:28:06 +0800

    还没有写玩

--- js/vr.js
+++ js/vr.js
@@ -5,6 +5,17 @@ $(document).ready(function () {
 
     $(".info_right .toChinaSite.info_comment").click(function () {
         alert(this.href);
-    })
+    });
 
+    // $("#VrImgNav .glyphicon-menu-left").click(function () {
+    //    $("#VrImgNav  .VrImgNav_Img_First").toggle();
+    //     $("#VrImgNav  .VrImgNav_Img_Second").toggle();
+    //     // alert("hsdhhfh");
+    // });
+
+    $("#VrImgNav .glyphicon-menu-left , .glyphicon-menu-right , .circle_first , .circle_second").click(function () {
+        $("#VrImgNav  .VrImgNav_Img_First").toggle();
+        $("#VrImgNav  .VrImgNav_Img_Second").toggle();
+        // alert("hsdhhfh");
+    });
 });
